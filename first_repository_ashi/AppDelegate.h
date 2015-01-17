573a177f2c76f455d0c6abfaa1645ab39873c9be
AShi <aunshilord@yahoo.com>
2015-01-17 11:26:04 +0300

    create project

---
+++ first_repository_ashi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  first_repository_ashi
+//
+//  Created by robert on 1/17/15.
+//  Copyright (c) 2015 ashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ first_repository_ashi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  first_repository_ashi
+//
+//  Created by robert on 1/17/15.
+//  Copyright (c) 2015 ashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
